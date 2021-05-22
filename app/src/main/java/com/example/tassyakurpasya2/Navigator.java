c073c2accd43dce20d49960c9552abb16f690da9
Achapasya2109 <acha.pasya2109@gmail.com>
2021-05-22 11:33:29 +0700

    permulaan

---
+++ app/src/main/java/com/example/tassyakurpasya2/Navigator.java
@@ -0,0 +1,106 @@
+package com.example.tassyakurpasya2;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Menu;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class Navigator extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    Fragment fragment;
+    FragmentTransaction transaction;
+    DrawerLayout drawer;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigator);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toogle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+        toogle.syncState();
+
+        navigationView.getMenu().getItem(0).setChecked(true);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.navigator, menu);
+        return true;
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                fragment = new HomeFragment();
+                break;
+            case R.id.nav_daily:
+                fragment = new DailyFragment();
+                break;
+            case R.id.nav_gallery:
+            fragment = new GalleryFragment();
+            break;
+            case R.id.nav_musikvideo:
+            fragment = new MusikVIdeoFragment();
+            break;
+            case R.id.nav_profil:
+                fragment = new ProfilFragment();
+                break;
+        }
+        if (fragment != null) {
+            transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.flayout_menu, fragment);
+            transaction.commit();
+        }
+        drawer.closeDrawers();
+        return true;
+    }
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawers();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/tassyakurpasya2/fragments/PageFragment3.java
@@ -0,0 +1,45 @@
+package com.example.tassyakurpasya2.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.tassyakurpasya2.R;
+import com.example.tassyakurpasya2.Navigator;
+import com.example.tassyakurpasya2.Navigator;
+
+public class PageFragment3 extends Fragment  {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        ViewGroup rootView = (ViewGroup)inflater
+                .inflate(R.layout.page3,container
+                        ,false);
+        ImageView tangan = rootView.findViewById(R.id.tangan);
+        tangan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tangan(v);
+            }
+        });
+
+
+        return rootView;
+
+    }
+    public void tangan(View view) {
+        Intent intent = new Intent(getActivity(), Navigator.class);
+        startActivity(intent);
+    }
+}
